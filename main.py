65d3c146ac9e022c72af70cc5741837625ab9ce1
christianAral <20018793+christianAral@users.noreply.github.com>
2024-01-10 12:14:15 -0500

    remove /test route in app Rename main script to main.py

--- main.py
+++ main.py
@@ -10,30 +10,6 @@ def serve_index():
 @app.route('/<path:filename>')
 def serve_static(filename):
     return send_from_directory('Interface', filename)
-
-@app.route('/test',methods=['GET','POST','PUT'])
-def refreshRules():
-    print(f'We detected a {request.method!a} request.')
-
-    try:
-        match request.method:
-            case "GET":
-                    resp = {'message':'Nothing to see here!'}
-                    return jsonify(resp),200
-            case "POST":
-                    # data = request.get_json()
-                    data = request.json
-                    print(data)
-                    # resp = {'message':f'looks good to me! this is what we received: {data}'}
-                    resp = {'message':f'looks good to me! this is what we received: {data}'}
-                    return jsonify(resp),200
-            case "PUT":
-                    data = request.get_json()
-                    resp = {'message':f'looks good to me! this is what we received: {data}'}
-                    return jsonify(resp),200            
-    except Exception as e:
-        return jsonify(e), 500
-
     
 @app.route('/refreshFirewallRules',methods=['GET'])
 def refresh_Firewall_Rules():
