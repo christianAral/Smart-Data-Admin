7798a87d379ca50c22beb711a75399b072511a19
christianAral <20018793+christianAral@users.noreply.github.com>
2024-02-02 10:12:46 -0500

    Moved Logger to Standalone class

--- SmartData.py
+++ SmartData.py
@@ -2,8 +2,8 @@ from typing import Any
 import os
 import json
 import requests
-import datetime
-import base64
+
+from Logger import Logger
 
 try:
     from azure.identity import InteractiveBrowserCredential
@@ -33,7 +33,7 @@ class SmartDataAdmin():
         self.globalConfig = type("", (), {})()
         self.kvConfig = type("", (), {})()
         self.sqlConfig = type("", (), {})()
-        self.logger = type("", (), {})()
+        # self.logger = type("", (), {})()
 
     # def _checkEnvironVariables(self):
         self.globalConfig.tenantid = os.environ.get('AZURE_TENANT_ID')
@@ -71,16 +71,8 @@ class SmartDataAdmin():
             credential=self._credential
         )
 
-    # def _setupBlobLogClient(self):
         logConfig = json.loads(self.kv.get_secret('blobLogConfig').value)
-        self.logger.config = logConfig
-        self.logger.blob_service_client = BlobServiceClient(
-            account_url=f"https://{self.logger.config['storageAccount']}.blob.core.windows.net", 
-            credential=self._credential
-        )
-        self.logger.container_client = self.logger.blob_service_client.get_container_client(
-            container=self.logger.config['container']
-        )
+        self.logger = Logger(self._credential,logConfig,self.upn)
 
     # def _setupSQLClient(self):
         sqlConfig = json.loads(self.kv.get_secret('firewallRuleManagerConfig').value)
@@ -91,53 +83,6 @@ class SmartDataAdmin():
         self.sqlConfig.curr_baseline = []
 
         self.sql = SqlManagementClient(self._credential, SUBSCRIPTION_ID)
-
-    def log(self,type:str,message:str):
-        timestamp = datetime.datetime.utcnow().isoformat(timespec='seconds')
-        if isinstance(message,dict):
-            message = str(message)
-        msg_tab_escaped = message.replace('\t','    ')
-        bMessage = (f"{timestamp}\t{type}\t{self.upn}\t{msg_tab_escaped}\n").encode()
-        
-        log_blob = self.logger.blob_service_client.get_blob_client(
-            self.logger.config['container'], 
-            f"SDAdmin/SDAdmin_{timestamp[:10]}.log"
-        )
-        if not log_blob.exists():
-            log_blob.create_append_blob()
-            headers = '\t'.join(['timestamp','type','user','message']) + '\n'
-            log_blob.append_block(headers.encode())
-
-        log_blob.append_block(bMessage)
-
-    def _tsv2dict(self,tsv:str) -> dict:
-        lines = tsv.split('\n')
-        header = lines[0].split('\t')
-        data_lines = lines[1:-1]
-
-        result = []
-        for line in data_lines:
-            columns = line.split('\t')
-            row_dict = {header[i] if i < len(header) else f'Field{i}': col for i, col in enumerate(columns)}
-            result.append(row_dict)
-
-        return result
-    
-    def list_log_file_names(self,b64:bool=True):
-        filenames = [b for b in self.logger.container_client.list_blob_names(name_starts_with='SDAdmin/SDAdmin',)]
-        
-        if b64:
-            return [base64.b64encode(fname.encode()).decode() for fname in filenames]
-        else:
-            return filenames 
-    
-    def get_log_file(self,filename,b64:bool=True):
-        if b64:
-            filename = base64.b64decode(filename.encode()).decode()
-        blobClient = self.logger.container_client.get_blob_client(filename)
-        blobStr = blobClient.download_blob().readall().decode('utf-8')
-        blobDict = self._tsv2dict(blobStr)
-        return blobDict
 
     def get_firewall_rules(self,refresh=True):
         if refresh==True or not hasattr(self.sqlConfig,'curr_firewall'):
@@ -224,7 +169,7 @@ class SmartDataAdmin():
 
         for instr in instructions['changedRow']:
             try:
-                self.log('firewallChange',instr)
+                self.logger.log('firewallChange',instr)
                 self._del_rule(instr)
                 self._add_rule(instr)
                 resp['success']['update'] += 1
@@ -232,14 +177,14 @@ class SmartDataAdmin():
                 resp['failure']['update'] += 1
         for instr in instructions['deletedRow']:
             try:
-                self.log('firewallRemove',instr)
+                self.logger.log('firewallRemove',instr)
                 self._del_rule(instr)
                 resp['success']['remove'] += 1
             except HttpResponseError:
                 resp['failure']['remove'] += 1
         for instr in instructions['addedRow']:
             try:
-                self.log('firewallAdd',instr)
+                self.logger.log('firewallAdd',instr)
                 self._add_rule(instr)
                 resp['success']['add'] += 1
             except HttpResponseError:
