089519f551ff41155f9375bf73aa748e2fa6ab94
christianAral <20018793+christianAral@users.noreply.github.com>
2024-02-07 15:40:33 -0500

    fixed version checker to query the correct path

--- CheckVersion.py
+++ CheckVersion.py
@@ -1,10 +1,9 @@
 import requests
+from pathlib import Path
 
 def CheckVersion():
-    try:
-        with open('version','r') as f:
-            content = f.read()
-        resp = requests.get('https://raw.githubusercontent.com/christianAral/Smart-Data-Admin/main/version').text
-        return (content == resp)
-    except:
-        return True
+    _VERSION_PATH = Path(__file__).with_name("version")
+    with open(_VERSION_PATH,'r') as f:
+        content = f.read()
+    resp = requests.get('https://raw.githubusercontent.com/christianAral/Smart-Data-Admin/main/version').text
+    return (content == resp)
